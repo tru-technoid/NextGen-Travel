28c2bc65c099bc659355c2837da94e37279b6a1a
tru-technoid <trusharpatel605@gmail.com>
2024-11-06 14:42:32 +0530

    Update The Backends.

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -4,25 +4,29 @@ import { createTour, deleteTour, getSingleTour, updateTour, getAllTour, getTourB
 const router = express.Router()
 
 //Create New Tour
+
 router.post("/", createTour);
 
 //Update Tour
+
 router.put("/:id", updateTour);
 
 //Delete New Tour
+
 router.delete("/:id", deleteTour);
 
 //Get Single Tour
+
 router.get("/:id", getSingleTour);
 
 //Get all tours
+
 router.get("/", getAllTour);
 
 //Get tour by search
+
 router.get("/search/getTourBySearch", getTourBySearch);
-
 router.get("/search/getFeaturedTours", getFeaturedTour);
-
 router.get("/search/getTourCount", getTourCount);
 
 export default router;
